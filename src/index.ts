562b5231752a99fd833ad869f889617649db7cca
SaudTauqeer <saudtauqeer381@gmail.com>
2023-10-26 06:40:48 +0500

    init with webpack and aframe and build/dev npm scripts for a aframe scene

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const scene = document.querySelector("a-scene");
+  if (scene) {
+    // Your A-Frame initialization logic here
+    // For example, creating a new entity:
+    const box = document.createElement("a-box");
+    box.setAttribute("position", "0 2 -5");
+    box.setAttribute("rotation", "0 45 0");
+    box.setAttribute("color", "red");
+    scene.appendChild(box);
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, "dist/**/*")],
+    }),
+  ],
+};
